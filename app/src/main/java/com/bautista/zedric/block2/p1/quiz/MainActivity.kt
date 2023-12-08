aa322b36ab23c57cbc89610b5ab87b23edb74c6e
saydrake <zema.bautista.up@phinmaed.com>
2023-12-08 15:24:53 +0800

    a

--- app/src/main/java/com/bautista/zedric/block2/p1/quiz/MainActivity.kt
+++ app/src/main/java/com/bautista/zedric/block2/p1/quiz/MainActivity.kt
@@ -2,7 +2,9 @@ package com.bautista.zedric.block2.p1.quiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,5 +13,19 @@ class MainActivity : AppCompatActivity() {
 
         val firstNumber: EditText = findViewById(R.id.insrt_First)
         val secondNumber: EditText = findViewById(R.id.insrt_Second)
+        val btnMultiply: Button = findViewById(R.id.btn_Multiply)
+        val txtNumber: TextView = findViewById(R.id.txt_Number)
+
+        txtNumber.text = ("ey! ").toString()
+
+
+        btnMultiply.setOnClickListener {
+            txtNumber.text = (firstNumber.text.toString().toInt()*secondNumber.text.toString().toInt()).toString()
+
+        }
+
+
+
+
     }
 }
