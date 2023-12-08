86a5e4ece2c9a352dc2156ed247aa9df420eedf7
Milbourne1 <jona.mamaril.up@phinmaed.com>
2023-12-08 14:42:03 +0800

    zed

--- app/src/main/java/com/bautista/zedric/block2/p1/quiz/MainActivity.kt
+++ app/src/main/java/com/bautista/zedric/block2/p1/quiz/MainActivity.kt
@@ -2,10 +2,14 @@ package com.bautista.zedric.block2.p1.quiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val firstNumber: EditText = findViewById(R.id.insrt_First)
+        val secondNumber: EditText = findViewById(R.id.insrt_Second)
     }
 }
